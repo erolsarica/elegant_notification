ea2b817d41bd798a14cef2253d931486feb0a149
erolsarica <erolsarica@gmail.com>
2022-08-22 10:14:02 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/elegant_notification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.elegant_notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,166 @@
+import 'package:elegant_notification/elegant_notification.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 300.0,left: 8.0, right: 8.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 3/1,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          children: [
+            ElevatedButton(
+              child: Text ("Yeni Sürüm"),
+              style: ElevatedButton.styleFrom(textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              backgroundColor: Colors.orange),
+              onPressed: (){
+                ElegantNotification(
+                  title: Text("Yeni Sürüm"),
+                  description:  Text("Uygulamanız için yeni bir sürüm mevcut. Lütfen Güncelleyin!"),
+                  icon: Icon(
+                    Icons.access_alarm,
+                    color: Colors.orange,
+                  ),
+                  progressIndicatorColor: Colors.orange,
+                ).show(context);
+              },
+            ),
+            ElevatedButton(
+              child: Text ("Güncelle"),
+              style: ElevatedButton.styleFrom(textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  backgroundColor: Colors.green),
+              onPressed: (){
+                ElegantNotification.success(
+                    title:  Text("Güncelleme"),
+                    description:  Text("Uygulamanız Güncellendi!")
+                ).show(context);
+
+              },
+            ),
+            ElevatedButton(
+              child: Text ("Bilgi"),
+              style: ElevatedButton.styleFrom(textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  backgroundColor: Colors.blueAccent),
+              onPressed: (){
+                  ElegantNotification.info(
+                      title: Text("Bilgi"),
+                      description: Text("Bu bir bilgilendirme uyarısıdır!")
+                  ).show(context);
+                },
+            ),
+            ElevatedButton(
+              child: Text ("Hata"),
+              style: ElevatedButton.styleFrom(textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  backgroundColor: Colors.red),
+              onPressed: (){
+                ElegantNotification.error(
+                    title:  Text("Hata"),
+                    description:  Text("Lütfen kontrol ediniz!")
+                ).show(context);
+
+              },
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+/*
+Column(
+children: [
+Row(
+children: [
+Padding(
+padding: const EdgeInsets.only(left: 25,right: 8.0, top: 8.0, bottom: 8.0),
+child: ElevatedButton(
+child: Text ("Yeni Sürüm"),
+onPressed: (){
+ElegantNotification(
+title: Text("Yeni Sürüm"),
+description:  Text("Uygulamanız için yeni bir sürüm mevcut. Lütfen Güncelleyin!"),
+icon: Icon(
+Icons.access_alarm,
+color: Colors.orange,
+),
+progressIndicatorColor: Colors.orange,
+).show(context);
+},
+),
+),
+Padding(
+padding: const EdgeInsets.all(8.0),
+child: ElevatedButton(
+child: Text ("Güncelle"),
+onPressed: (){
+ElegantNotification.success(
+title:  Text("Güncelleme"),
+description:  Text("Uygulamanız Güncellendi!")
+).show(context);
+
+},
+),
+),
+
+Row(
+children: [
+Padding(
+padding: const EdgeInsets.all(8.0),
+child: ElevatedButton(
+child: Text ("Bilgi"),
+onPressed: (){
+ElegantNotification.info(
+title:  Text("Bilgi"),
+description:  Text("Bu bir bilgilendirme uyarısıdır!")
+).show(context);
+
+},
+),
+),
+],
+),
+],
+),
+],
+),
+
+ */
+
+
